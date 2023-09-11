6d8ccad313c450756141d8e092291f559a0cafd9
peppegerace <peppegerace47@gmail.com>
2023-09-11 17:02:09 +0200

    [add] conditions for divisible numbers by 3, 5 &15 [add] comments in js [edit] name in the :root css [add] h1 in html

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
+// seleziono elemento contenitore
 const boxesContainer = document.querySelector('.boxes');
+// ciclo le operazioni di creazione
 for (let i = 1; i <= 100; i++) {
+  // creo un nuovo elemento
   const box = document.createElement('div');
+  //  aggiungo una classe per l'elemento creato
   box.classList.add('box');
-  
+  // inserisco il valore all'interno dell'elemento creato
+  box.append(i);
+  // aggiungo l'elemento creato al contenitore
+  boxesContainer.append(box);
+
   console.log(i)
 
-  boxesContainer.append(box);
+  // aggiungo le conndizioni per i numeri divisibili per 3
+  if (!(i % 3)) {
+    box.classList.add('bg-fizz');
+    box.innerHTML = 'fizz';
+  }
+  // aggiungo le conndizioni per i numeri divisibili per 5
+  if (!(i % 5)) {
+    box.classList.add('bg-buzz');
+    box.innerHTML = 'buzz';
+  }
+  // aggiungo le conndizioni per i numeri divisibili per 3 e per 5
+  if (!(i % 15)) {
+    box.classList.add('bg-fizzbuzz');
+    box.innerHTML = 'fizzbuzz';
+  }
+
+  console.log(box.innerHTML)
 }
 
